1ebbaa6a87ec85e77d0cf8602e186d0c2275d2f0
Jesús Pedro Rodriguez <jesuspedro.rodriguez@unir.net>
2026-05-16 01:41:12 +0200

    Refactor code structure for improved readability and maintainability

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5vflis4ef28"
+    "revision": "0.1jhrsvgg75o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
