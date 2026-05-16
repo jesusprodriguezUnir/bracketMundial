2eb65c91011c37bd9e21f2810d3e5c967c378677
Jesús Pedro Rodriguez <jesusprodriguez@gmail.com>
2026-05-16 10:10:50 +0200

    feat: update permissions in settings.local.json and revise cache revision in sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.mf75ul4ggf"
+    "revision": "0.9kvsj7vui6o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
