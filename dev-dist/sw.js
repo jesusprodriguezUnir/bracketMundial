b9de5fce71b2cd9bad1d076145a72e6b5c096eab
Jesús Pedro Rodriguez <jesusprodriguez@gmail.com>
2026-05-17 14:09:23 +0200

    fix: corregir errores tipográficos en descripciones y metadatos en index.html

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.s2o9blptdc8"
+    "revision": "0.2h375u49fp"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
